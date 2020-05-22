2e949859d55001fad358a885ffeb7702d9760752
Lincoln Machado <morpheu@msn.com>
2020-05-22 01:58:26 -0300

    Refactor and stylization

--- src/Main.js
+++ src/Main.js
@@ -1,51 +1,60 @@
-import React, { useState } from "react";
+import React from "react";
+import styled from "styled-components";
 import groupBy from "lodash.groupby";
-import { nanoid } from "nanoid";
-import { format } from "date-fns";
+import ThingItem from "./ThingItem";
+import plus from "./assets/plus.svg";
 
-const Main = ({ tasks, onNewTaskDone, onRemoveTask }) => {
-  const [obs, setObs] = useState("");
-  const byCategory = groupBy(tasks, (t) => t.category);
+const Container = styled.div`
+  background-color: ${p => p.theme.background};
+  color: ${p => p.theme.text};
+  overflow-y: auto;
+  padding: 1em;
+`;
 
+const EmptyContainer = styled.p`
+  text-align: center;
+  padding: 16px;
+  font-size: 2rem;
+  line-height: 4rem;
+`;
+
+const SmallCheckButton = styled.img`
+  height: 32px;
+  width: 32px;
+  vertical-align: middle;
+`;
+
+const Category = styled.div``;
+const CategoryName = styled.h3`
+  font-size: 0.8rem;
+  color: ${p => p.theme.categoryColor};
+`;
+
+const Main = ({ things, removeThing, onNewThingDone }) => {
+  const byCategory = groupBy(things, t => t.category);
   return (
-    <div>
-      {Object.keys(byCategory).map((c) => (
-        <div key={c}>
-          <h4>{c}</h4>
-          {byCategory[c].map((t) => (
-            <div key={t.id}>
-              <div>
-                {t.text}
-                <button onClick={() => onRemoveTask(t.id)}>deletar</button>
-              </div>
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  onNewTaskDone(t.id, { id: nanoid(), obs, date: Date.now() });
-                  setObs("");
-                }}
-              >
-                <input
-                  type="text"
-                  value={obs}
-                  onChange={(e) => setObs(e.target.value)}
-                />
-              </form>
-              <table>
-                <tbody>
-                  {t.logs.map((l) => (
-                    <tr key={l.id}>
-                      <td>{format(l.date, "dd/MM/yyyy hh:mm")}</td>
-                      <td>{l.obs}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+    <Container>
+      {things.length === 0 && (
+        <EmptyContainer>
+          Não tem nada aqui. Clique em{" "}
+          <SmallCheckButton src={plus} alt="Adicionar tarefa" /> para começar.
+        </EmptyContainer>
+      )}
+      {Object.keys(byCategory).map(c => (
+        <Category key={c}>
+          <CategoryName>{c}</CategoryName>
+          {byCategory[c].map(t => (
+            <ThingItem
+              key={t.id}
+              {...t}
+              removeThing={removeThing}
+              onNewThingDone={onNewThingDone}
+            />
           ))}
-        </div>
+        </Category>
       ))}
-    </div>
+      {/* <pre>{JSON.stringify(byCategory, null, 1)}</pre> */}
+    </Container>
   );
 };
 
